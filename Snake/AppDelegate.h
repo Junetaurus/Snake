f275f8540d240ce0846628c0025efbb67127219b
Taurus-GitHub <zj0376@163.com>
2017-06-09 17:58:11 +0800

    Initial Commit

---
+++ Snake/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Snake
+//
+//  Created by ZhangJun on 2017/6/9.
+//  Copyright © 2017年 ANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Snake/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Snake
+//
+//  Created by ZhangJun on 2017/6/9.
+//  Copyright © 2017年 ANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
